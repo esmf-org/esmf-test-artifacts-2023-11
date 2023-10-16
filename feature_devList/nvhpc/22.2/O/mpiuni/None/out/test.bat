98f85296d55b1dd5c2d84ccf50f99b50f633afaa
Gerhard Theurich <theurich@sourcespring.net>
2023-10-16 16:15:28 -0600

    action=collect dir=nvhpc_22.2_mpiuni_O_feature_devList branch=feature_devList hash=v8.6.0b09-16-gcd68da5 phase=test

---
+++ feature_devList/nvhpc/22.2/O/mpiuni/None/out/test.bat
@@ -0,0 +1,29 @@
+#!/bin/sh -l
+#PBS -N test.bat
+#PBS -l walltime=3:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load python/3.7.9 cmake/3.22.0
+module load nvhpc/22.2 
+module load netcdf/4.8.1
+
+set -x
+export ESMF_DIR=/glade/scratch/theurich/ESMF-Nightly-Testing/nvhpc_22.2_mpiuni_O_feature_devList/esmf
+export ESMF_COMPILER=nvhpc
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/glade/scratch/theurich/ESMF-Nightly-Testing/nvhpc_22.2_mpiuni_O_feature_devList/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /glade/scratch/theurich/ESMF-Nightly-Testing/nvhpc_22.2_mpiuni_O_feature_devList/module-test.log
+export WORK_ROOT=/glade/scratch/theurich/ESMF-Nightly-Testing/nvhpc_22.2_mpiuni_O_feature_devList
+export TEMP_ROOT=/glade/scratch/theurich/ESMF-Nightly-Testing/nvhpc_22.2_mpiuni_O_feature_devList
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+make install 2>&1| tee $WORK_ROOT/install.log
+make all_tests 2>&1| tee $WORK_ROOT/test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
