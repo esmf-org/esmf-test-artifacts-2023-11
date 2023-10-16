e23670b6ef12f033f3ad1abdae0db1538109ea41
Gerhard Theurich <theurich@sourcespring.net>
2023-10-16 15:54:32 -0600

    action=collect dir=gfortran_10.1.0_mpt_O_feature_devList branch=feature_devList hash=v8.6.0b09-16-gcd68da5 phase=test

---
+++ feature_devList/gfortran/10.1.0/O/mpt/2.23/out/build.bat
@@ -0,0 +1,30 @@
+#!/bin/sh -l
+#PBS -N build.bat
+#PBS -l walltime=1:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load cmake/3.22.0
+module load gnu/10.1.0 mpt/2.23
+module load netcdf/4.7.4
+module load python/3.7.9
+
+set -x
+export ESMPY_DATA_DIR="/glade/work/theurich/esmf-test-data/grids"
+export ESMF_DIR=/glade/scratch/theurich/ESMF-Nightly-Testing/gfortran_10.1.0_mpt_O_feature_devList/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpt
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /glade/scratch/theurich/ESMF-Nightly-Testing/gfortran_10.1.0_mpt_O_feature_devList/module-build.log
+export WORK_ROOT=/glade/scratch/theurich/ESMF-Nightly-Testing/gfortran_10.1.0_mpt_O_feature_devList
+export TEMP_ROOT=/glade/scratch/theurich/ESMF-Nightly-Testing/gfortran_10.1.0_mpt_O_feature_devList
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 36 2>&1| tee $WORK_ROOT/build.log
