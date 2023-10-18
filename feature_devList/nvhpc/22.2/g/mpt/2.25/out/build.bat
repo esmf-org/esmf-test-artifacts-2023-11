16b409d7b7d68cb8be758be808003008394173f2
Gerhard Theurich <theurich@sourcespring.net>
2023-10-18 07:26:35 -0600

    action=collect dir=nvhpc_22.2_mpt_g_feature_devList branch=feature_devList hash=v8.6.0b09-20-g8d1ee58 phase=test

---
+++ feature_devList/nvhpc/22.2/g/mpt/2.25/out/build.bat
@@ -0,0 +1,28 @@
+#!/bin/sh -l
+#PBS -N build.bat
+#PBS -l walltime=1:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load python/3.7.9 cmake/3.22.0
+module load nvhpc/22.2 mpt/2.25
+module load netcdf/4.8.1
+
+set -x
+export ESMF_DIR=/glade/scratch/theurich/ESMF-Nightly-Testing/nvhpc_22.2_mpt_g_feature_devList/esmf
+export ESMF_COMPILER=nvhpc
+export ESMF_COMM=mpt
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /glade/scratch/theurich/ESMF-Nightly-Testing/nvhpc_22.2_mpt_g_feature_devList/module-build.log
+export WORK_ROOT=/glade/scratch/theurich/ESMF-Nightly-Testing/nvhpc_22.2_mpt_g_feature_devList
+export TEMP_ROOT=/glade/scratch/theurich/ESMF-Nightly-Testing/nvhpc_22.2_mpt_g_feature_devList
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 36 2>&1| tee $WORK_ROOT/build.log
