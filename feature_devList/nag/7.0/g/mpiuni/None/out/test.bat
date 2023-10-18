1f68bac8698e6b35b310d4fe37aa4cd8e0619bef
Gerhard Theurich <theurich@sourcespring.net>
2023-10-18 05:29:43 -0600

    action=collect dir=nag_7.0_mpiuni_g_feature_devList branch=feature_devList hash=v8.6.0b09-20-g8d1ee58 phase=test

---
+++ feature_devList/nag/7.0/g/mpiuni/None/out/test.bat
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load CMake/3.24.3
+module load compiler/nag/7.0 
+module load tool/netcdf/4.6.1/nag
+
+set -x
+export ESMF_DIR=/project/esmf/theurich/ESMF-Nightly-Testing/nag_7.0_mpiuni_g_feature_devList/esmf
+export ESMF_COMPILER=nag
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/project/esmf/theurich/ESMF-Nightly-Testing/nag_7.0_mpiuni_g_feature_devList/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /project/esmf/theurich/ESMF-Nightly-Testing/nag_7.0_mpiuni_g_feature_devList/module-test.log
+export WORK_ROOT=/project/esmf/theurich/ESMF-Nightly-Testing/nag_7.0_mpiuni_g_feature_devList
+export TEMP_ROOT=/project/esmf/theurich/ESMF-Nightly-Testing/nag_7.0_mpiuni_g_feature_devList
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+make install 2>&1| tee $WORK_ROOT/install.log
+make all_tests 2>&1| tee $WORK_ROOT/test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
