e6dc88d3f3ec18ab641960b203c6eb4ed1e3b387
Gerhard Theurich <theurich@sourcespring.net>
2023-10-23 04:08:28 -0600

    action=collect dir=gfortran_13.1.0_mpt_g_develop branch=develop hash=v8.6.0b10-17-g1bed7e4 phase=test

---
+++ develop/gfortran/13.1.0/g/mpt/2.25/out/esmpy_install.bat
@@ -0,0 +1,25 @@
+#!/bin/sh
+module load cmake/3.22.0
+module load gnu/13.1.0 mpt/2.25
+module load netcdf/4.9.1
+module load python/3.7.9
+
+set -x
+export ESMPY_DATA_DIR="/glade/work/theurich/esmf-test-data/grids"
+export ESMF_DIR=/glade/scratch/theurich/ESMF-Nightly-Testing/gfortran_13.1.0_mpt_g_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpt
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+cd /glade/scratch/theurich/ESMF-Nightly-Testing/gfortran_13.1.0_mpt_g_develop/esmf
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd /glade/scratch/theurich/ESMF-Nightly-Testing/gfortran_13.1.0_mpt_g_develop
+rm -rf esmpy_venv
+python3 -m venv esmpy_venv
+. esmpy_venv/bin/activate
+cd /glade/scratch/theurich/ESMF-Nightly-Testing/gfortran_13.1.0_mpt_g_develop/esmf/src/addon/esmpy
+python3 -m pip install . 2>&1| tee /glade/scratch/theurich/ESMF-Nightly-Testing/gfortran_13.1.0_mpt_g_develop/esmpy-install.log
+make download 2>&1| tee /glade/scratch/theurich/ESMF-Nightly-Testing/gfortran_13.1.0_mpt_g_develop/esmpy-download.log
+deactivate
