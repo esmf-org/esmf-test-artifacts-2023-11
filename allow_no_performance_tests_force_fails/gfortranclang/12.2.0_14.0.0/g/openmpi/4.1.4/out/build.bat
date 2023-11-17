2971a74dcc3d955b3ce91ff0fe45f9b531d9bbf6
Bill Sacks <sacks@ucar.edu>
2023-11-17 09:25:05 -0700

    action=collect dir=gfortranclang_12.2.0_14.0.0_openmpi_g_allow_no_performance_tests_force_fails branch=allow_no_performance_tests_force_fails hash=v8.7.0b01-7-g9bbfa5a phase=build

---
+++ allow_no_performance_tests_force_fails/gfortranclang/12.2.0_14.0.0/g/openmpi/4.1.4/out/build.bat
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load gfortran-12.2.0_clang-14.0.0 4.1.4
+module load netcdf-4.9.0
+
+set -x
+export ESMF_TESTPERFORMANCE=OFF
+export ESMF_DIR=/Users/sacks/projects/scratch/esmf-testing/gfortranclang_12.2.0_14.0.0_openmpi_g_allow_no_performance_tests_force_fails/esmf
+export ESMF_COMPILER=gfortranclang
+export ESMF_COMM=openmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /Users/sacks/projects/scratch/esmf-testing/gfortranclang_12.2.0_14.0.0_openmpi_g_allow_no_performance_tests_force_fails/module-build.log
+export WORK_ROOT=/Users/sacks/projects/scratch/esmf-testing/gfortranclang_12.2.0_14.0.0_openmpi_g_allow_no_performance_tests_force_fails
+export TEMP_ROOT=/Users/sacks/projects/scratch/esmf-testing/gfortranclang_12.2.0_14.0.0_openmpi_g_allow_no_performance_tests_force_fails
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 8 2>&1| tee $WORK_ROOT/build.log
